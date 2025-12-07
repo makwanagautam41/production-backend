65cc150797541cb516e83c14ebeb23ee038b96ea
Gautam Makwana <makwanagautam411@gmail.com>
2025-12-07 18:20:44 +0530

    add pm2 ecosystem.config file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports  = {
+    apps:[
+        {
+            name:"prod-backend",
+            script:"./dist/server.js",
+            instances="max",
+            exec_mode:"cluster",
+            env:{
+                NODE_ENV:"development"
+            },
+            env_production:{
+                NODE_ENV:"production"
+            },
+        },
+    ],
+};
