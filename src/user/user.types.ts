1f5b7a05118850a8b0aab4a92011402fadfb37ea
Gautam Makwana <makwanagautam411@gmail.com>
2025-12-08 21:42:03 +0530

    fix the type of user

--- src/user/user.types.ts
+++ src/user/user.types.ts
@@ -3,5 +3,8 @@ export interface User {
   name: string;
   email: string;
   password: string;
-  profileImage: string;
+  profileImage: {
+    public_id: string | null;
+    secure_url: string | null;
+  };
 }
